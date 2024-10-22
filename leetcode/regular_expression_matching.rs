d5e8e89fd75488b318d71ade277ecbd9ed48c894
Vũ Minh Thanh <vuminhthanh8501@gmail.com>
2024-10-22 16:54:54 +0700

    Regular Expression Matching

--- leetcode/regular_expression_matching.rs
+++ leetcode/regular_expression_matching.rs
@@ -1,20 +1,37 @@
-use super::Solution;
+struct Solution;
 
 #[allow(dead_code)]
 impl Solution {
-    // TODO:
+    /// Runtime: 0ms     | 100.00%
+    /// Memory : 2.07 MB | 86.26%
     pub fn is_match(s: String, p: String) -> bool {
         if s.is_empty() || p.is_empty() {
             return false;
         }
 
         let (m, n) = (s.len(), p.len());
-        let mut matrix: Vec<Vec<bool>> = vec![vec![false; n + 1]; m + 1];
-        matrix[0][0] = true;
+        let (s_bytes, p_bytes) = (s.as_bytes(), p.as_bytes());
 
-        for i in (2..=n).step_by(2) {
-            if p.as_bytes()[i] == b'*' && matrix[0][i - 2] {
-                matrix[0][i] = true
+        let mut matrix = vec![vec![false; n + 1]; m + 1];
+        matrix[0][0] = true;
+        for i in 1..n {
+            if p_bytes[i] == b'*' && matrix[0][i - 1] == true {
+                matrix[0][i + 1] = matrix[0][i - 1];
+            }
+        }
+
+        for i in 0..m {
+            for j in 0..n {
+                if p_bytes[j] == b'.' || p_bytes[j] == s_bytes[i] {
+                    matrix[i + 1][j + 1] = matrix[i][j];
+                } else if p_bytes[j] == b'*' {
+                    if p_bytes[j - 1] != b'.' && p_bytes[j - 1] != s_bytes[i] {
+                        matrix[i + 1][j + 1] = matrix[i + 1][j - 1];
+                    } else {
+                        matrix[i + 1][j + 1] =
+                            matrix[i + 1][j] || matrix[i][j + 1] || matrix[i + 1][j - 1];
+                    }
+                }
             }
         }
 
@@ -24,7 +41,7 @@ impl Solution {
 
 #[test]
 fn test_regular_expression_matching_1() {
-    assert!(Solution::is_match(String::from("aa"), String::from("a")))
+    assert!(!Solution::is_match(String::from("aa"), String::from("a")))
 }
 
 #[test]
